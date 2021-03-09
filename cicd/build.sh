bcdd8b49809741b3e6fd93cc6933fbf220a18b99
Etienne Dilocker <etienne@dilocker.de>
2021-03-09 10:25:58 +0100

    gh-1 fix quoting issue around docker build arg

--- cicd/build.sh
+++ cicd/build.sh
@@ -5,4 +5,4 @@ set -e
 local_repo=${LOCAL_REPO?Variable LOCAL_REPO is required}
 model_name=${MODEL_NAME?Variable MODEL_NAME is required}
 
-docker build "--build-arg MODEL_NAME=$model_name" -t "$local_repo" .
+docker build --build-arg "MODEL_NAME=$model_name" -t "$local_repo" .
