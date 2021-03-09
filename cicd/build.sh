582c8b21b9329bafc0cc82d0f657210fcba87c5b
Etienne Dilocker <etienne@dilocker.de>
2021-03-09 10:21:34 +0100

    gh-1 add model matrix .travis file

---
+++ cicd/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+local_repo=${LOCAL_REPO?Variable LOCAL_REPO is required}
+model_name=${MODEL_NAME?Variable MODEL_NAME is required}
+
+docker build "--build-arg MODEL_NAME=$model_name" -t "$local_repo" .
