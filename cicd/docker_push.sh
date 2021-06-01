47f43feed3467734f3bcaae7c96448c5f40f2f5e
Etienne Dilocker <etienne@dilocker.de>
2021-06-01 13:51:33 +0200

    fix empty model name

--- cicd/docker_push.sh
+++ cicd/docker_push.sh
@@ -15,7 +15,7 @@ git_hash=
 pr=
 remote_repo=${REMOTE_REPO?Variable REMOTE_REPO is required}
 model_name=${MODEL_NAME?Variable MODEL_NAME is required}
-original_model_name=
+original_model_name=$model_name
 docker_username=${DOCKER_USERNAME?Variable DOCKER_USERNAME is required}
 docker_password=${DOCKER_PASSWORD?Variable DOCKER_PASSWORD is required}
 
@@ -33,7 +33,6 @@ function init() {
     # a model tag name was specified to overwrite the model name. This is the
     # case, for example, when the original model name contains characters we
     # can't use in the docker tag
-    original_model_name="$MODEL_NAME"
     model_name="$MODEL_TAG_NAME"
   fi
 
