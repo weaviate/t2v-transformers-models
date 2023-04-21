1c392585324d618e5467e40350bae62354148544
Alex Cannan <alexfcannan@gmail.com>
2023-04-21 09:36:24 -0400

    vectorize asynchronously with ThreadPoolExecutor

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def startup_event():
 
 @app.get("/.well-known/live", response_class=Response)
 @app.get("/.well-known/ready", response_class=Response)
-def live_and_ready(response: Response):
+async def live_and_ready(response: Response):
     response.status_code = status.HTTP_204_NO_CONTENT
 
 
