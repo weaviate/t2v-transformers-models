334d3afb73a7c2c4b96141850195107068bed509
kl-thamm <lennart@kleinaber.de>
2023-06-28 16:02:08 +0200

    improve readability

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from meta import Meta
 
 
 app = FastAPI()
-vec : Vectorizer
-meta_config : Meta
-logger = getLogger('uvicorn')
+vec: Vectorizer
+meta_config: Meta
+logger = getLogger("uvicorn")
 
 
 @app.on_event("startup")
@@ -20,16 +20,22 @@ def startup_event():
     cuda_per_process_memory_fraction = 1.0
     if "CUDA_PER_PROCESS_MEMORY_FRACTION" in os.environ:
         try:
-            cuda_per_process_memory_fraction = float(os.getenv("CUDA_PER_PROCESS_MEMORY_FRACTION"))
+            cuda_per_process_memory_fraction = float(
+                os.getenv("CUDA_PER_PROCESS_MEMORY_FRACTION")
+            )
         except ValueError:
-            logger.error(f"Invalid CUDA_PER_PROCESS_MEMORY_FRACTION (should be between 0.0-1.0)")
+            logger.error(
+                f"Invalid CUDA_PER_PROCESS_MEMORY_FRACTION (should be between 0.0-1.0)"
+            )
     if 0.0 <= cuda_per_process_memory_fraction <= 1.0:
-        logger.info(f"CUDA_PER_PROCESS_MEMORY_FRACTION set to {cuda_per_process_memory_fraction}")
-    cuda_support=False
-    cuda_core=""
+        logger.info(
+            f"CUDA_PER_PROCESS_MEMORY_FRACTION set to {cuda_per_process_memory_fraction}"
+        )
+    cuda_support = False
+    cuda_core = ""
 
     if cuda_env is not None and cuda_env == "true" or cuda_env == "1":
-        cuda_support=True
+        cuda_support = True
         cuda_core = os.getenv("CUDA_CORE")
         if cuda_core is None or cuda_core == "":
             cuda_core = "cuda:0"
@@ -37,15 +43,32 @@ def startup_event():
     else:
         logger.info("Running on CPU")
 
-    # Batch text tokenization enabled by default
-    direct_tokenize = False
-    transformers_direct_tokenize = os.getenv("T2V_TRANSFORMERS_DIRECT_TOKENIZE")
-    if transformers_direct_tokenize is not None and transformers_direct_tokenize == "true" or transformers_direct_tokenize == "1":
-        direct_tokenize = True
+    # Split input in sentences by default
+    # And perform tokenization in batches
+    shall_split_in_sentences = True
+    env_shall_split_in_sentences = os.getenv("T2V_SHALL_SPLIT_IN_SENTENCES")
+    if (
+        env_shall_split_in_sentences is not None
+        and env_shall_split_in_sentences == "false"
+        or env_shall_split_in_sentences == "0"
+    ):
+        shall_split_in_sentences = False
 
-    meta_config = Meta('./models/model')
-    vec = Vectorizer('./models/model', cuda_support, cuda_core, cuda_per_process_memory_fraction,
-                     meta_config.getModelType(), meta_config.get_architecture(), direct_tokenize)
+    if not shall_split_in_sentences:
+        logger.warn(
+            f"Configured not to split input into sentences. Inputs will be truncated if they exceed the models context length."
+        )
+
+    meta_config = Meta("./models/model")
+    vec = Vectorizer(
+        "./models/model",
+        cuda_support,
+        cuda_core,
+        cuda_per_process_memory_fraction,
+        meta_config.getModelType(),
+        meta_config.get_architecture(),
+        shall_split_in_sentences,
+    )
 
 
 @app.get("/.well-known/live", response_class=Response)
@@ -66,8 +89,6 @@ async def read_item(item: VectorInput, response: Response):
         vector = await vec.vectorize(item.text, item.config)
         return {"text": item.text, "vector": vector.tolist(), "dim": len(vector)}
     except Exception as e:
-        logger.exception(
-            'Something went wrong while vectorizing data.'
-        )
+        logger.exception("Something went wrong while vectorizing data.")
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {"error": str(e)}
--- vectorizer.py
+++ vectorizer.py
@@ -19,7 +19,8 @@ from transformers import (
 # limit transformer batch size to limit parallel inference, otherwise we run
 # into memory problems
 MAX_BATCH_SIZE = 25  # TODO: take from config
-DEFAULT_POOL_METHOD="masked_mean"
+DEFAULT_POOL_METHOD = "masked_mean"
+
 
 class VectorInputConfig(BaseModel):
     pooling_strategy: str
@@ -29,40 +30,60 @@ class VectorInput(BaseModel):
     text: str
     config: Optional[VectorInputConfig] = None
 
+
 class Vectorizer:
     model: AutoModel
     tokenizer: AutoTokenizer
     cuda: bool
     cuda_core: str
     model_type: str
-    direct_tokenize: bool
+    shall_split_in_sentences: bool
     executor: ThreadPoolExecutor
 
-    def __init__(self, model_path: str, cuda_support: bool, cuda_core: str, cuda_per_process_memory_fraction: float, model_type: str, architecture: str, direct_tokenize: bool):
+    def __init__(
+        self,
+        model_path: str,
+        cuda_support: bool,
+        cuda_core: str,
+        cuda_per_process_memory_fraction: float,
+        model_type: str,
+        architecture: str,
+        shall_split_in_sentences: bool,
+    ):
         self.cuda = cuda_support
         self.cuda_core = cuda_core
         self.cuda_per_process_memory_fraction = cuda_per_process_memory_fraction
         self.model_type = model_type
-        self.direct_tokenize = direct_tokenize
+        self.shall_split_in_sentences = shall_split_in_sentences
 
-        self.model_delegate: HFModel = ModelFactory.model(model_type, architecture, cuda_support, cuda_core)
+        self.model_delegate: HFModel = ModelFactory.model(
+            model_type, architecture, cuda_support, cuda_core
+        )
         self.model = self.model_delegate.create_model(model_path)
 
         if self.cuda:
             self.model.to(self.cuda_core)
             if self.cuda_per_process_memory_fraction:
-                torch.cuda.set_per_process_memory_fraction(self.cuda_per_process_memory_fraction)
-        self.model.eval() # make sure we're in inference mode, not training
+                torch.cuda.set_per_process_memory_fraction(
+                    self.cuda_per_process_memory_fraction
+                )
+        self.model.eval()  # make sure we're in inference mode, not training
 
         self.tokenizer = self.model_delegate.create_tokenizer(model_path)
 
         self.executor = ThreadPoolExecutor()
 
-        nltk.data.path.append('./nltk_data')
+        nltk.data.path.append("./nltk_data")
 
-    def tokenize(self, text:str):
-        return self.tokenizer(text, padding=True, truncation=True, max_length=500,
-                add_special_tokens = True, return_tensors="pt")
+    def tokenize(self, text: str):
+        return self.tokenizer(
+            text,
+            padding=True,
+            truncation=True,
+            max_length=500,
+            add_special_tokens=True,
+            return_tensors="pt",
+        )
 
     def get_embeddings(self, batch_results):
         return self.model_delegate.get_embeddings(batch_results)
@@ -75,20 +96,17 @@ class Vectorizer:
 
     def _vectorize(self, text: str, config: VectorInputConfig):
         with torch.no_grad():
-            if self.direct_tokenize:
-                # create embeddings without tokenizing text
-                tokens = self.tokenize(text)
-                if self.cuda:
-                    tokens.to(self.cuda_core)
-                batch_results = self.get_batch_results(tokens, text)
-                batch_sum_vectors = self.pool_embedding(batch_results, tokens, config)
-                return batch_sum_vectors.detach()
-            else:
-                # tokenize text
-                sentences = sent_tokenize(' '.join(text.split(),))
+            if self.shall_split_in_sentences:
+                # Split text into sentences
+                sentences = sent_tokenize(
+                    " ".join(
+                        text.split(),
+                    )
+                )
                 num_sentences = len(sentences)
                 number_of_batch_vectors = math.ceil(num_sentences / MAX_BATCH_SIZE)
                 batch_sum_vectors = 0
+                # Performer transformer tokenization and embedding in batches of sentences
                 for i in range(0, number_of_batch_vectors):
                     start_index = i * MAX_BATCH_SIZE
                     end_index = start_index + MAX_BATCH_SIZE
@@ -96,16 +114,31 @@ class Vectorizer:
                     tokens = self.tokenize(sentences[start_index:end_index])
                     if self.cuda:
                         tokens.to(self.cuda_core)
-                    batch_results = self.get_batch_results(tokens, sentences[start_index:end_index])
-                    batch_sum_vectors += self.pool_embedding(batch_results, tokens, config)
+                    batch_results = self.get_batch_results(
+                        tokens, sentences[start_index:end_index]
+                    )
+                    batch_sum_vectors += self.pool_embedding(
+                        batch_results, tokens, config
+                    )
                 return batch_sum_vectors.detach() / num_sentences
 
+            else:
+                # create embeddings without splitting text into sentences
+                # this will truncate texts that are too long for the model
+                tokens = self.tokenize(text)
+                if self.cuda:
+                    tokens.to(self.cuda_core)
+                batch_results = self.get_batch_results(tokens, text)
+                batch_sum_vectors = self.pool_embedding(batch_results, tokens, config)
+                return batch_sum_vectors.detach()
+
     async def vectorize(self, text: str, config: VectorInputConfig):
-        return await asyncio.wrap_future(self.executor.submit(self._vectorize, text, config))
+        return await asyncio.wrap_future(
+            self.executor.submit(self._vectorize, text, config)
+        )
 
 
 class HFModel:
-
     def __init__(self, cuda_support: bool, cuda_core: str):
         super().__init__()
         self.model = None
@@ -132,7 +165,9 @@ class HFModel:
         if pooling_method == "cls":
             return self.get_embeddings(batch_results)[:, 0, :].sum(0)
         elif pooling_method == "masked_mean":
-            return self.pool_sum(self.get_embeddings(batch_results), tokens['attention_mask'])
+            return self.pool_sum(
+                self.get_embeddings(batch_results), tokens["attention_mask"]
+            )
         else:
             raise Exception(f"invalid pooling method '{pooling_method}'")
 
@@ -147,8 +182,12 @@ class HFModel:
 
     def get_sum_embeddings_mask(self, embeddings, input_mask_expanded):
         if self.cuda:
-            sum_embeddings = torch.sum(embeddings * input_mask_expanded, 1).to(self.cuda_core)
-            sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9).to(self.cuda_core)
+            sum_embeddings = torch.sum(embeddings * input_mask_expanded, 1).to(
+                self.cuda_core
+            )
+            sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9).to(
+                self.cuda_core
+            )
             return sum_embeddings, sum_mask
         else:
             sum_embeddings = torch.sum(embeddings * input_mask_expanded, 1)
@@ -156,14 +195,17 @@ class HFModel:
             return sum_embeddings, sum_mask
 
     def pool_sum(self, embeddings, attention_mask):
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
-        sum_embeddings, sum_mask = self.get_sum_embeddings_mask(embeddings, input_mask_expanded)
+        input_mask_expanded = (
+            attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
+        )
+        sum_embeddings, sum_mask = self.get_sum_embeddings_mask(
+            embeddings, input_mask_expanded
+        )
         sentences = sum_embeddings / sum_mask
         return sentences.sum(0)
 
 
 class DPRModel(HFModel):
-
     def __init__(self, architecture: str, cuda_support: bool, cuda_core: str):
         super().__init__(cuda_support, cuda_core)
         self.model = None
@@ -177,7 +219,7 @@ class DPRModel(HFModel):
         return self.model
 
     def get_batch_results(self, tokens, text):
-        return self.model(tokens['input_ids'], tokens['attention_mask'])
+        return self.model(tokens["input_ids"], tokens["attention_mask"])
 
     def pool_embedding(self, batch_results, tokens, config: VectorInputConfig):
         # no pooling needed for DPR
@@ -185,7 +227,6 @@ class DPRModel(HFModel):
 
 
 class T5Model(HFModel):
-
     def __init__(self, cuda_support: bool, cuda_core: str):
         super().__init__(cuda_support, cuda_core)
         self.model = None
@@ -205,7 +246,7 @@ class T5Model(HFModel):
         return batch_results["encoder_last_hidden_state"]
 
     def get_batch_results(self, tokens, text):
-        input_ids, attention_mask = tokens['input_ids'], tokens['attention_mask']
+        input_ids, attention_mask = tokens["input_ids"], tokens["attention_mask"]
 
         target_encoding = self.tokenizer(
             text, padding="longest", max_length=500, truncation=True
@@ -216,16 +257,17 @@ class T5Model(HFModel):
         else:
             labels = torch.tensor(labels)
 
-        return self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+        return self.model(
+            input_ids=input_ids, attention_mask=attention_mask, labels=labels
+        )
 
 
 class ModelFactory:
-
     @staticmethod
     def model(model_type, architecture, cuda_support: bool, cuda_core: str):
-        if model_type == 't5':
+        if model_type == "t5":
             return T5Model(cuda_support, cuda_core)
-        elif model_type == 'dpr':
+        elif model_type == "dpr":
             return DPRModel(architecture, cuda_support, cuda_core)
         else:
             return HFModel(cuda_support, cuda_core)
