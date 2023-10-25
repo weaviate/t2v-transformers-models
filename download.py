fb9d535a46b483d1f3f863f8a85578a03b3c8aa1
Marcin Antas <antas.marcin@gmail.com>
2023-10-25 21:17:42 +0200

    Fix embedding generation for T5 models

--- download.py
+++ download.py
@@ -8,8 +8,10 @@ from transformers import (
     AutoTokenizer,
     AutoConfig,
 )
+from sentence_transformers import SentenceTransformer
 
 
+model_dir = './models/model'
 model_name = os.getenv('MODEL_NAME', None)
 force_automodel = os.getenv('FORCE_AUTOMODEL', False)
 if not model_name:
@@ -22,21 +24,28 @@ if force_automodel:
 
 print(f"Downloading model {model_name} from huggingface model hub")
 config = AutoConfig.from_pretrained(model_name)
-if config.architectures and not force_automodel:
-    print(f"Using class {config.architectures[0]} to load model weights")
-    mod = __import__('transformers', fromlist=[config.architectures[0]])
-    try:
-        klass_architecture = getattr(mod, config.architectures[0])
-        model = klass_architecture.from_pretrained(model_name)
-    except AttributeError:
-        print(f"{config.architectures[0]} not found in transformers, fallback to AutoModel")
-        model = AutoModel.from_pretrained(model_name)
+model_type = config.to_dict()['model_type']
+
+if model_type is not None and model_type == "t5":
+    SentenceTransformer(model_name, cache_folder=model_dir)
+    with open(f"{model_dir}/model_name", "w") as f:
+        f.write(model_name.replace("/", "_"))
 else:
-    model = AutoModel.from_pretrained(model_name)
+    if config.architectures and not force_automodel:
+        print(f"Using class {config.architectures[0]} to load model weights")
+        mod = __import__('transformers', fromlist=[config.architectures[0]])
+        try:
+            klass_architecture = getattr(mod, config.architectures[0])
+            model = klass_architecture.from_pretrained(model_name)
+        except AttributeError:
+            print(f"{config.architectures[0]} not found in transformers, fallback to AutoModel")
+            model = AutoModel.from_pretrained(model_name)
+    else:
+        model = AutoModel.from_pretrained(model_name)
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-model.save_pretrained('./models/model')
-tokenizer.save_pretrained('./models/model')
+    model.save_pretrained(model_dir)
+    tokenizer.save_pretrained(model_dir)
 
 nltk.download('punkt', download_dir='./nltk_data')
