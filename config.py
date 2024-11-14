715dd7eaa5e9c9fbdd15b9a8fe154129cc4741c4
Marcin Antas <antas.marcin@gmail.com>
2024-11-14 20:27:21 +0100

    Add support for authorization bearer token

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 import os
+from typing import List
 
 TRUST_REMOTE_CODE = os.getenv("TRUST_REMOTE_CODE", False)
+
+
+def get_allowed_tokens() -> List[str] | None:
+    if (
+        tokens := os.getenv("AUTHENTICATION_ALLOWED_TOKENS", "").strip()
+    ) and tokens != "":
+        return tokens.strip().split(",")
